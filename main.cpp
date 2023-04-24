b007a43a9a9512624a0c5a90aacfb4ea4558db63
nikitochka2012 <131687859+nikitochka2012@users.noreply.github.com>
2023-04-24 21:17:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,101 +1,21 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
 #include <iomanip>
-//14 вариант
+#include "Header.h"
+
 using namespace std;
-void print(int (& arr)[10][10]) 
-{
- for (int i{}; i < 10; i++) 
- {
- for (int j{}; j < 10; j++) 
- {
- cout << setw(5) << arr[i][j];
- }
- cout << endl;
- }
- cout << endl;
-}
-void swap(int &first, int &second)
-{
- int buf = first;
- first = second;
- second = buf;
-}
-void createRandomMatrix(int (& matrix)[10][10])
-{
- for (int i{}; i < 10; i++)
- {
- for (int j{}; j < 10; j++)
- {
- matrix[i][j] = rand() % 21 - 10;
- }
- }
-};
-void sumIf(int (& arr)[10][10], int &sum)
-{
- for (int i{}; i < 10; i++)
- {
- for (int j{}; j < 10; j++)
- {
- if (i >= j && arr[i][j] < 0)
- {
- sum += arr[i][j];
- }
- }
- }
-};
-void replaceToVector(int (&arr)[10][10], int vector[10])
-{
- for (int i{}; i < 10; i++)
- {
- bool ok = 1;
- for (int j{}; j < 10; j++)
- {
- if (arr[i][j] != 0)
- {
- ok = 0;
- break;
- }
- }
- if (ok) 
- {
- for (int j{}; j < 10; j++) 
- {
- arr[i][j] = vector[j];
- }
- }
- }
-}
-void replaceOnTask(int (&matrix)[10][10])
-{
- for (int i{}; i < 10; i++)
- {
- for (int j{}; j < 10; j++)
- {
- for (int p = j + 1; p < 10; p++)
- {
- if ((matrix[i][j] == 0) && (matrix[i][p] != 0))
- {
- swap(matrix[i][j], matrix[i][p]);
- }
- else if ((matrix[i][j] >= 0) && (matrix[i][p] < 0))
- {
- swap(matrix[i][j], matrix[i][p]);
- }
- }
- }
- }
-}
-int main(){
- int matrix[10][10], sum{}, vector [10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
- srand(time(0));
- createRandomMatrix(matrix);
- print(matrix);
- replaceToVector(matrix, vector);
- replaceOnTask(matrix);
- print(matrix);
- sumIf(matrix, sum);
- cout << "Summa = " << sum << endl;
- return 0;
+
+int main() {
+	system("chcp 1251");
+	int matrix[8][8];
+	int row{}, col{}, rowUnder{}, colUnder{};
+
+	createRandomMatrix(matrix);
+	print(matrix);
+
+	int minOn = minValueOnSideDiagonal(matrix, row, col), minUnder = minValueUnderSideDiagonal(matrix, rowUnder, colUnder);
+	
+	cout << "Минимальное значение на побочной диагонали: " << minOn << " Строка " << row + 1 << " Столбец " << col + 1 << endl;
+	cout << "Минимальное значение под побочной диагональю: " << minUnder << " Строка " << rowUnder + 1 << " Столбец " << colUnder + 1 << endl;
+	replaceOnTask(matrix, row, col, rowUnder, colUnder);
+	print(matrix);
 }
